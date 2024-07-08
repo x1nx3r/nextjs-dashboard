ae46159e9b5086b7bbe2d18bb668f41bbd230a97
x1nx3r <monmega110@gmail.com>
2024-07-08 21:51:39 +0700

    chore: Fix formatting and add missing semicolon in nav-links.tsx

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,5 +1,5 @@
-'use client';
- 
+'use client'
+
 import {
   UserGroupIcon,
   HomeIcon,
@@ -7,13 +7,22 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import clsx from 'clsx';
- 
-// ...
- 
+import clsx from 'clsx'
+
+// Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
+const links = [
+  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  {
+    name: 'Invoices',
+    href: '/dashboard/invoices',
+    icon: DocumentDuplicateIcon,
+  },
+  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+];
+
 export default function NavLinks() {
   const pathname = usePathname();
- 
   return (
     <>
       {links.map((link) => {
@@ -22,11 +31,10 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className={clsx(
-              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+            className={clsx('flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
                 'bg-sky-100 text-blue-600': pathname === link.href,
-              },
+              }
             )}
           >
             <LinkIcon className="w-6" />
